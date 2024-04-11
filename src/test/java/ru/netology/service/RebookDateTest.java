0cb36806bccdcf76e015016db720bc8c3c9fdb67
Denwin <biscooter@mail.ru>
2024-04-11 22:04:15 +0600

    addallure

--- src/test/java/ru/netology/service/RebookDateTest.java
+++ src/test/java/ru/netology/service/RebookDateTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import com.codeborne.selenide.Condition;
 import org.openqa.selenium.Keys;
 
@@ -17,6 +17,16 @@ class RebookDateTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setupAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
@@ -35,6 +45,29 @@ class RebookDateTest {
         $(".checkbox__box").click();
         $$("button").find(Condition.exactText("Запланировать")).click();
         $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("div.notification__content > button").click();
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+    @Test
+    void shouldSuccessfulBookAndRebookMeeting2() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 5;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[placeholder='Город']").setValue(validUser.getCity());
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[name='phone']").setValue("212212");
+        $(".checkbox__box").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("В11стреча успешно запланирована на " + firstMeetingDate));
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
         $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
         $$("button").find(Condition.exactText("Запланировать")).click();
