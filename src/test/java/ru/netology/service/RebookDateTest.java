46d3863e36f67a23ad93aa2c444a6edad433734b
Denwin <biscooter@mail.ru>
2024-04-16 16:33:57 +0600

    delete 2T

--- src/test/java/ru/netology/service/RebookDateTest.java
+++ src/test/java/ru/netology/service/RebookDateTest.java
@@ -52,28 +52,4 @@ class RebookDateTest {
         $("div.notification__content > button").click();
         $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
-
-    @Test
-    void shouldSuccessfulBookAndRebookMeeting2() {
-        var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 5;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $("[placeholder='Город']").setValue(validUser.getCity());
-        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
-        $("[name='name']").setValue(validUser.getName());
-        $("[name='phone']").setValue("212212");
-        $(".checkbox__box").click();
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("В11стреча успешно запланирована на " + firstMeetingDate));
-        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
-        $$("button").find(Condition.exactText("Запланировать")).click();
-        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("div.notification__content > button").click();
-        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
-    }
-
 }
